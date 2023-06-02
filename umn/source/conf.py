1b739427b9a9fd06465a79894cd5398ae2221198
Tino Schr <tino.schreiber@t-systems.com>
2023-06-02 13:24:03 +0200

    Update tox.ini && conf.py file (#30)
    Update tox.ini && conf.py file
    
    Performed-by: gitea/infra/otc-metadata/tools/generate_doc_confpy.py
    
    Reviewed-by: SebastianGode

--- umn/source/conf.py
+++ umn/source/conf.py
@@ -28,6 +28,15 @@ project = 'Software Repository for Containers'
 otcdocs_repo_name = 'opentelekomcloud-docs/software-repository-container'
 # Those variables are required for edit/bug links
 
+# Those variables are needed for indexing into OpenSearch
+otcdocs_doc_environment = 'public'
+otcdocs_doc_link = '/software-repository-container/umn/'
+otcdocs_doc_title = 'User Guide'
+otcdocs_doc_type = 'umn'
+otcdocs_service_category = 'container'
+otcdocs_service_title = 'Software Repository for Containers'
+otcdocs_service_type = 'swr'
+
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
